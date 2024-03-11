4bd00e90ad0a396ec85eb6ebb0eebce4819c60a4
VeridisEst <apstra@hotmail.com>
2024-03-11 04:45:11 -0400

    course started!

--- docs/lint.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-find ./ -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "../PDFs/$(basename ${0%.md}.pdf)"' {} \;
-
-#find ./ -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "${0%.md}.pdf"' {} \;
